305b1c8d8ffa1174ebad3d1f959da03d6d15ef83
Gustavo Okuyama <gustavo.okuyama@ifood.com.br>
2023-09-03 16:51:55 -0300

    feat: add store

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gubtos/gosubmoduleexample
+
+go 1.21.0
---
+++ store.go
@@ -0,0 +1,45 @@
+package gosubmoduleexample
+
+import "errors"
+
+var ErrNotFound = errors.New("not found")
+
+type Data struct {
+	ValA string
+	ValB string
+}
+
+type Logger interface {
+	Log(Data)
+}
+
+type Store struct {
+	logger  Logger
+	dataMap map[string]Data
+}
+
+func NewStore(logger Logger) *Store {
+	return &Store{
+		logger:  logger,
+		dataMap: make(map[string]Data),
+	}
+}
+
+func (c *Store) StoreData(a string, b string) error {
+	newData := Data{
+		ValA: a,
+		ValB: b,
+	}
+	c.dataMap[a] = newData
+	c.logger.Log(newData)
+
+	return nil
+}
+
+func (c *Store) RetrieveData(a string) (Data, error) {
+	if data, ok := c.dataMap[a]; ok {
+		return data, nil
+	}
+
+	return Data{}, ErrNotFound
+}
